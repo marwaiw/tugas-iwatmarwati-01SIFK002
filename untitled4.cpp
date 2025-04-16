aaf9360c067bb6b5e5bc71b3776c7fc13269f49b
marwaiw <iiiwatmarwati02@gmail.com>
2025-04-16 13:19:52 +0700

    Add files via upload
    tugas algoritma pemrograman latihan c++

---
+++ untitled4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main () {
+	string nama, kelas, NIM, hobby;
+	cout<< "masukan nama:" << endl;
+	cin >> nama;
+	cout << "masukan kelas:" << endl;
+	cin >> kelas;
+	cout << "masukan NIM:" << endl;
+	cin >> NIM;
+	cout << "masukan hobby:" << endl;
+	cin >> hobby;
+	cout << "data diri" <<endl;
+	cout << "nama:" << nama <<endl;
+	cout << "kelas:" << kelas <<endl;
+	cout << "NIM:" << NIM <<endl;
+	cout << "hobby:" << hobby <<endl;
+}
